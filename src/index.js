ddcfdc6f1b2f4a15c8d16e8cbaae74e2db2b9c2b
Oliver Acevedo <acevedo.oliver@gmail.com>
2020-04-30 18:51:46 -0500

    add ramda, style changes, yoy stat

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 import { Line, Bar } from "react-chartjs-2";
 import database from './firebase';
+import{ propOr } from 'ramda';
 import "./style.css";
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = { checkpoints: [] };
+    this.state = {
+      checkpoints: [],
+      loading: true,
+    };
   }
 
   componentDidMount() {
@@ -15,19 +19,21 @@ class App extends Component {
     database.collection("checkpoints").orderBy('date').get()
     .then(querySnapshot => {
       querySnapshot.forEach(doc => checkpoints.push(doc.data()));
-      this.setState({ checkpoints });
+      this.setState({ checkpoints, loading: false });
     })
     .catch(err => console.error(err));
   }
+  formatNum = num => parseInt(num.replace(',','').replace(',',''));
+
   render() {
-    const { checkpoints } = this.state;
-    const formatNum = num => parseInt(num.replace(',','').replace(',',''));
+    const { checkpoints, loading } = this.state;
+    
     const data = {
       labels: checkpoints.map(entry => entry.date.toDate().toLocaleDateString()),
       datasets: [
         {
           label: "2019",
-          data: checkpoints.map(entry => formatNum(entry['2020'])),
+          data: checkpoints.map(entry => this.formatNum(entry['2020'])),
           backgroundColor: "rgba(255, 99, 132, 0.2)",
           borderColor: "rgba(255, 99, 132, 1)",
           borderWidth: 1,
@@ -36,7 +42,7 @@ class App extends Component {
         },
         {
           label: "2020",
-          data: checkpoints.map(entry => formatNum(entry['2019'])),
+          data: checkpoints.map(entry => this.formatNum(entry['2019'])),
           backgroundColor: "rgba(255, 0, 0, 0.4)",
           borderColor: "rgba(255, 0, 0, 1)",
           borderWidth: 1,
@@ -57,7 +63,7 @@ class App extends Component {
             const data2019 = data.datasets[tooltipItems[0].datasetIndex].data[tooltipItems[0].index];
             const data2020 = data.datasets[0].data[tooltipItems[0].index];
             const percentValue = Math.round(data2019/data2020 * 100);
-            return `${percentValue} %`;
+            return tooltipItems[0].datasetIndex === 0 ? null : `${percentValue} %`;
           }
         }
       },
@@ -83,10 +89,15 @@ class App extends Component {
       }
     };
 
+    const getCheckpoints = (year, index) => this.formatNum(propOr('0', year, checkpoints[checkpoints.length - index]));
+   
+    const yoyChange = 100 - getCheckpoints(2019, 1)/ getCheckpoints(2020, 1) * 100
+    const dailyChange = getCheckpoints(2019, 1)/ getCheckpoints(2020, 1)/getCheckpoints(2019, 58)/ getCheckpoints(2020, 58)
     return (
-      <div>
-        <h1>TSA Checkpoints</h1>
-        <p>Total Traveler Throughput</p>
+      <div class="container">
+        <h1>TSA Passenger Throughput</h1>
+        {/* <span className="change-text">Daily change: {!loading && Math.round(dailyChange)}%</span><span className="arrow-down"></span> */}
+        <span className="change-text">YoY change: {!loading && Math.round(yoyChange)}% </span><span className="arrow-down"></span>
         <Bar id="chart" data={data} options={options} />
       </div>
     );
